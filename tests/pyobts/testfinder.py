005b8fffa6807ca6c2691689026c1f14f798e744
Steven Lariau <obs145628@gmail.com>
2020-01-03 09:41:52 +0100

    [GENERAL][TESTS] Cleaner test name and organization

--- tests/pyobts/testfinder.py
+++ tests/pyobts/testfinder.py
@@ -1,6 +1,19 @@
 from .testsuite import UnitTest
 import os
 
+def split_all_path(p):
+
+    res = []
+    while True:
+        p, last = os.path.split(p)
+        if last != '':
+            res.append(last)
+        else:
+            if p != '':
+                res.append(p)
+            break
+    return res[::-1]
+
 class FilesTestFinder:
 
     def __init__(self):
@@ -20,7 +33,7 @@ class FilesTestFinder:
             for name in files:
                 f = os.path.join(root, name)
                 if self.is_valid_file(f):
-                    res += [f]
+                    res += [self.get_unit(f, d)]
             
         return res
             
@@ -30,7 +43,7 @@ class FilesTestFinder:
         for name in os.listdir(d):
             f = os.path.join(d, name)
             if os.path.isfile(f) and self.is_valid_file(f):
-                res += [f]
+                res += [self.get_unit(f, d)]
         return res
 
     def add_dir(self, d, recursive=False):
@@ -42,14 +55,20 @@ class FilesTestFinder:
         res = []
         for d, recursive in self.dirs:
             files = self.list_recur(d) if recursive else self.list_notrecur(d)
-            res += [self.get_unit(f) for f in files]
+            res += files
+
+        res.sort(key = lambda x: x.name)
 
         return res
 
 
-    def get_unit(self, f):
+    def get_unit(self, f, base_dir):
         f = os.path.abspath(f)
-        name = os.path.basename(f)
+        base_dir = os.path.abspath(base_dir)
+
+        rel_path = os.path.relpath(f, base_dir)
+        name = '.'.join(split_all_path(rel_path))
+
         return UnitTest(name, f)
 
     def is_valid_file(self, f):
